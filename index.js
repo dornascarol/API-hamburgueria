f24de97822e4896491b4e78a101eb0b8c1288a4c
dornascarol <carolinafdornas@gmail.com>
2023-07-05 00:52:08 -0300

    criando rota get com query params

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const port = 3000
 const app = express() 
 
 app.get("/order", (request, response) => {
-    return response.send("Order")
+    
+    const { order, clienteName, price, orderStatus } = request.query  //destructuring assignment
+
+    return response.json({order, clienteName, price, orderStatus})
 })
+
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
